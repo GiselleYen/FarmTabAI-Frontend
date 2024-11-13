517c9fe7352e3735907beb8d71b1ca358e3aa257
GiselleYen <giselleyen02@gmail.com>
2024-11-13 12:07:30 +0800

    updated commite

--- farmtab_ai/android/app/src/main/kotlin/com/example/farmtab_ai/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.farmtab_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ farmtab_ai_frontend/android/app/src/main/kotlin/com/example/farmtab_ai_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.farmtab_ai_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
